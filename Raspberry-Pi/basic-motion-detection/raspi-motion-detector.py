633a1bbb0260e570c0373c3d86286b9b04623e80
Blake Edwards <blakeedwards823@gmail.com>
2019-07-26 16:49:54 -0400

    removed framerate

--- Raspberry-Pi/basic-motion-detection/raspi-motion-detector.py
+++ Raspberry-Pi/basic-motion-detection/raspi-motion-detector.py
@@ -25,7 +25,7 @@ if args.get("video", None) is None:
 	# initialize the camera and grab a reference to the raw camera capture
 	camera = PiCamera()
 	camera.resolution = (640, 480)
-	camera.framerate = 32
+	#camera.framerate = 32
 	rawCapture = PiRGBArray(camera, size=(640, 480))
 	time.sleep(2.0)
 # otherwise, we are reading from a video file
