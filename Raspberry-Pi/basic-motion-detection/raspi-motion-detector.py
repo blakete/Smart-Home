361fff087c6cec641932139e940871b511c63e16
Blake Edwards <blakeedwards823@gmail.com>
2019-08-14 16:53:15 -0400

    changed 7 image capture to 1 for reset of reference

--- Raspberry-Pi/basic-motion-detection/raspi-motion-detector.py
+++ Raspberry-Pi/basic-motion-detection/raspi-motion-detector.py
@@ -44,7 +44,7 @@ firstFrame = None
 # initializing previous text
 previousText = "No-movement"
 # initializing movement counter to reset still frame reference
-referenceResetCount = 7
+referenceResetCount = 1
 count = 0
 
 print("[+] Starting security feed...")
